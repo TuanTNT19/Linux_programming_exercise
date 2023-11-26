19b3bef2f9943ec96b6901ddf12c3f15209ad7d8
TuanTNT19 <tuantnt08@gmail.com>
2023-11-26 15:38:28 +0700

    reconven programming

--- 02_file_system/BT4/main.c
+++ 02_file_system/BT4/main.c
@@ -12,7 +12,7 @@ int main() {
     }
 
 // Write some data to the end of the file
-    char data1[15]="Tuan deptrai 1\n";
+    char data1[15] = "Tuan deptrai 1\n";
     lseek(fd,0,SEEK_SET);
     if (write(fd, data1, strlen(data1)) == -1) {
         printf("Can not write 1\n");
@@ -23,10 +23,10 @@ int main() {
 
     return 0;
 }
+
 /* 
 - Cờ APPEND chỉ định dữ liệu luôn được ghi vào cuối tệp ( bất chấp lseek())
 + Trong VD trên, ta tạo trước file test.txt với dữ liệu mặc định là TEST APPEND
 - Nếu không dùng O_APPEND thì khi lseek() lên đầu file , dữ liệu mới sẽ ghi đè lên dữ liệu cũ -> File txt sau cùng chỉ có dữ liệu mới
 - Nếu dùng O_APPEND thì dù cho có lseek() thì dữ liệu mới vẫn mặc định ghi ở cuối file -> File txt sau cùng có đầy đủ cả dữ liệu mới và cũ
-
 */
